0dfc41984f3b349c288b6f1203270c59a685f673
KNightFuryX <60267521+KNightFuryX@users.noreply.github.com>
2022-05-07 18:19:21 +0545

    fixed urls

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,7 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login', views.login, name='login'),
-    path('logout', views.logout, name='logout'),
-    path('signup', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
